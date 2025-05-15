17d7ee10c3301e2f8c82faf9ad913cb3a5bf1a23
Klaus Förster <klaus.foerster@uibk.ac.at>
2025-05-15 16:42:15 +0200

    auf Kartenklick reagieren

--- main.js
+++ main.js
@@ -29,3 +29,8 @@ let layerControl = L.control.layers({
 L.control.scale({
     imperial: false,
 }).addTo(map);
+
+// auf Kartenklick reagieren
+map.on("click", function(evt) {
+    console.log(evt.latlng);
+})
