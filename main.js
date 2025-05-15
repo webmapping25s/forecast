bef0bd451ca7f17b3bd6972c0d898498f43c3fd2
Klaus Förster <klaus.foerster@uibk.ac.at>
2025-05-15 16:46:02 +0200

    Klick auf Innsbruck simulieren

--- main.js
+++ main.js
@@ -34,3 +34,11 @@ L.control.scale({
 map.on("click", function(evt) {
     console.log(evt.latlng);
 })
+
+// Klick auf Innsbruck simulieren
+map.fire("click", {
+    latlng: {
+        lat: ibk.lat,
+        lng: ibk.lng,
+    }
+})
