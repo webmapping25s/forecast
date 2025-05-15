cfc3678f7f8aa15382ddd75cd0de94ef8f8fc372
Klaus Förster <klaus.foerster@uibk.ac.at>
2025-05-15 16:50:27 +0200

    Funktion showForecast beginnen

--- main.js
+++ main.js
@@ -30,9 +30,15 @@ L.control.scale({
     imperial: false,
 }).addTo(map);
 
+// MET Norway Vorhersage visualisieren
+async function showForecast(latlng) {
+    console.log("Popup erzeugen bei:", latlng);
+}
+
 // auf Kartenklick reagieren
 map.on("click", function(evt) {
     console.log(evt.latlng);
+    showForecast(evt.latlng);
 })
 
 // Klick auf Innsbruck simulieren
