4dda168ea32d69f49b750baa9be9c780724b271e
Klaus Förster <klaus.foerster@uibk.ac.at>
2025-05-15 18:07:36 +0200

    Zeitstempel der Vorhersage als Tooltip anzeigen

--- main.js
+++ main.js
@@ -57,8 +57,8 @@ async function showForecast(latlng) {
     // Wettericons für die nächsten 24 Stunden in 3 Stunden Schritten
     for (let i=0; i <=24; i += 3) {
         let symbol = jsondata.properties.timeseries[i].data.next_1_hours.summary.symbol_code;
-     
-        markup += `<img src="icons/${symbol}.svg" style="width:32px">`;
+        let time = new Date(jsondata.properties.timeseries[i].time);
+        markup += `<img src="icons/${symbol}.svg" style="width:32px" title="${time.toLocaleString()}">`;
     }
 
     L.popup([
